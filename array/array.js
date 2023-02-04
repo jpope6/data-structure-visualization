feff78159c2acd4c7dfb0841ff3d0f697b1ef66c
jpope6 <imthepope@csu.fullerton.edu>
2023-02-04 13:33:21 -0800

    push button works

--- array/array.js
+++ array/array.js
@@ -8,16 +8,21 @@ let index = 0;
 for (let i = 0; i < 10; i++) {
     let div = document.createElement('div');
     let h1 = document.createElement('h1');
-    h1.innerHTML = '0';
     div.className = 'box';
     div.appendChild(h1);
     array_container.appendChild(div);
     array.push(div);
 }
 
-push_button.addEventListener('click', () => {
-    // Get the value from input
-    let value = document.getElementById('value').value;
-    array[index].children[0].innerHTML = value;
+// Push button will add value to array
+push_button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // if nothing is entered in input field, return
+    if (!document.getElementById('input').value) return;
+
+    let value = document.getElementById('input').value;
+    array[index].querySelector('h1').innerHTML = value;
+    document.getElementById('input').value = '';
     index++;
 })
