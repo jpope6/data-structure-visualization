61ea889956df497366e18eab07edf814bf0edf2b
jpope6 <imthepope@csu.fullerton.edu>
2023-02-04 17:27:55 -0800

    added some indexes

--- array/array.js
+++ array/array.js
@@ -2,17 +2,31 @@ let array_container = document.getElementById('array_container');
 let push_button = document.getElementById('push');
 let pop_button = document.getElementById('pop');
 
+let input_container = document.getElementById('input_container');
+
 let array = [];
 let index = 0;
 
 // Create 10 elements for array
 for (let i = 0; i < 10; i++) {
     let div = document.createElement('div');
-    let h1 = document.createElement('h1');
     div.className = 'box';
+
+    let h1 = document.createElement('h1');
     div.appendChild(h1);
+
     array_container.appendChild(div);
     array.push(div);
+    
+    let div_input = document.createElement('div');
+    div_input.className = 'input_box';
+
+    let h1_input = document.createElement('h1');
+    h1_input.className = 'input';
+    h1_input.innerHTML = i;
+    div_input.appendChild(h1_input);
+
+    input_container.appendChild(div_input);
 }
 
 // Push button will add value to array
