827e37f25d3b0d142bc639ee9d79b1341cda808c
jpope6 <imthepope@csu.fullerton.edu>
2023-02-14 15:50:18 -0800

    Binary search complete for now

--- algorithms/binary-search/binary-search.js
+++ algorithms/binary-search/binary-search.js
@@ -1,6 +1,7 @@
 let array = document.getElementById("array")
 let value = document.getElementById("input_value")
 let searchButton = document.getElementById("search_button")
+let resetButton = document.getElementById("reset_button")
 
 let arr = []
 
@@ -40,10 +41,9 @@ function addElements() {
 
 addElements()
 
-// This function will search for the given element in the array
+// This function will use binary search for the given element in the array
 async function search() {
     let target = value.value;
-    // if (!target) { return }
 
     let left = 0
     let right = arr.length - 1
@@ -51,14 +51,18 @@ async function search() {
     while (left <= right) {
         let mid = Math.floor((left + right) / 2)
         highlight(mid, target)
-        await sleep(500)
+        await sleep(1000)
 
         if (arr[mid] == target) {
+            erase(mid, true)
+            erase(mid, false)
             return mid
         } else if (arr[mid] < target) {
-            left = mid + 1 
+            erase(mid + 1, true)
+            left = mid + 1
         } else {
-            right = mid - 1 
+            erase(mid - 1, false)
+            right = mid - 1
         }
     }
 
@@ -77,6 +81,36 @@ function highlight(index, target) {
         dict[index].style.backgroundColor = "red"
     }
 }
+
+// This function will erase the elements on the left or right side of the array
+// depending on the binary search function
+function erase(index, direction) {
+    // if direction is false, erase the left side of the array
+    if (direction) {
+        for (let i = 0; i < index; i++) {
+            dict[i].style.color = "black";
+            dict[i].style.border = "1px solid black";
+            dict[i].style.background = "black"
+        }
+    } else {
+        // if direction is true, erase the right side of the array
+        for (let i = index + 1; i < arr.length; i++) {
+            dict[i].style.color = "black";
+            dict[i].style.border = "1px solid black";
+            dict[i].style.background = "black"
+        }
+    }
+}
+
+// Add event listener to the reset button 
+resetButton.addEventListener("click", () => {
+    for (let i = 0; i < arr.length; i++) {
+        dict[i].style.color = "white";
+        dict[i].style.border = "1px solid white";
+        dict[i].style.background = "black"
+    }
+    value.value = "";
+});
 
 // Sleep function to delay execution
 function sleep(ms) {
